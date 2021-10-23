a58cdbdc7820b20d24b982299118cf6e7098e597
shalabyelectronics <mhdshalabystar@gmail.com>
2021-10-23 21:15:23 +0300

    update the animation mode

--- levels.py
+++ levels.py
@@ -8,4 +8,11 @@ class Levels(Turtle):
         self.penup()
         self.level = 0
         self.goto(-400, 260)
+        self.level_update()
+
+    def level_update(self):
+        self.clear()
         self.write(f"Level : {self.level}", align="center", font=("Courier", 25, "normal"))
+
+    def increase_level(self):
+        self.level += 1
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from turtle import Screen
 from player import Player
 from levels import Levels
+import time
 
 screen = Screen()
 screen.setup(1000, 600)
 screen.title("Turtle Crossing Game")
+screen.tracer(0)
 player = Player()
 levels = Levels()
 
 screen.listen()
 screen.onkey(key="Up", fun=player.move_up)
+
+game_on = True
+
+while game_on:
+    time.sleep(0.1)
+    screen.update()
+    if player.ycor() > 250:
+        levels.increase_level()
+        levels.level_update()
+
+
 screen.exitonclick()
