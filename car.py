90c6d887a192704599d666ccb596b25263e4c6c9
shalabyelectronics <mhdshalabystar@gmail.com>
2021-10-23 23:16:02 +0300

    update car positions on the game board.

--- car.py
+++ car.py
@@ -1,29 +1,32 @@
 import turtle
 import random
 
-POSITION = [(0, 0), (20, 0), (40, 0)]
-
 
 class Car(turtle.Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
         turtle.colormode(255)
+        self.position = []
+        self.generate_position()
         self.segments = []
         self.create_car()
         self.car_position = self.segments[0]
-        self.car_position.goto(440, 0)
-        self.car_position.setheading(180)
 
+    def generate_position(self):
+        y_position = random.randint(-250, 250)
+        for pos in range(3):
+            self.position.append((500, y_position))
+            y_position -= 20
 
     def create_car(self):
-        for pos in range(len(POSITION)):
+        for pos in range(len(self.position)):
             color = random.randint(0, 255)
             car = turtle.Turtle()
             car.shape("square")
             car.penup()
             car.color((color, color, color))
-            car.goto(POSITION[pos])
+            car.goto(self.position[pos])
             self.segments.append(car)
 
     def move_car(self):
@@ -31,4 +34,5 @@ class Car(turtle.Turtle):
             new_x = self.segments[seg - 1].xcor()
             new_y = self.segments[seg - 1].ycor()
             self.segments[seg].goto(new_x, new_y)
+        self.segments[0].setheading(180)
         self.segments[0].forward(20)
